a5ffc6b1a557d6772463ec98ac0ad7380f47d8a7
Shota Hayashi <qsezy1@hotmail.com>
2018-05-20 02:04:11 +0900

    位置情報を取得するためのクラスを追加

---
+++ app/src/main/java/com/hayashi/android_trajectory/LocationListenerImpl.kt
@@ -0,0 +1,34 @@
+package com.hayashi.android_trajectory
+
+import android.location.Location
+import android.location.LocationListener
+import android.os.Bundle
+import com.hayashi.android_trajectory.LocationStarter.locationProvider
+import com.hayashi.android_trajectory.utility.MyLog
+import com.hayashi.android_trajectory.utility.MyMap
+
+object LocationListenerImpl : LocationListener {
+    private val l = MyLog("LocationListenerImpl")
+
+    // ロケーションプロバイダが利用可能になるとコールバックされるメソッド
+    override fun onProviderEnabled(provider: String) {
+        l.d("onProviderEnabled called : provider = $provider")
+        locationProvider = provider
+    }
+
+    // 位置情報が通知されるたびにコールバックされるメソッド
+    override fun onLocationChanged(location: Location) {
+        l.d("onLocationChanged called : location = $location")
+        MyMap.updateCurMarker(location)
+    }
+
+    // ロケーションステータスが変わるとコールバックされるメソッド
+    override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+        l.d("onStatusChanged called : provider = $provider, status = $status, extras = $extras")
+    }
+
+    //ロケーションプロバイダが利用不可能になるとコールバックされるメソッド
+    override fun onProviderDisabled(provider: String) {
+        l.d("onProviderDisabled called : provider = $provider")
+    }
+}
